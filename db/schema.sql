55b13f1d9aa7960c6816751c98584d94cfc090d3
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-10-08 08:59:39 -0400

    Adding Bootstrap

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-    id INTEGER AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     burger_name VARCHAR(30),
     devoured boolean,
     PRIMARY KEY(id)
